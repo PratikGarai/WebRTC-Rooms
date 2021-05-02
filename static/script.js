30a59713911a70a9cbaf59ca1606db0b18035d4c
PratikGarai <pratikgarai0208@gmail.com>
2021-05-02 13:10:46 +0530

    Videos linked

--- static/script.js
+++ static/script.js
@@ -1,13 +1,51 @@
 const socket = io('/');
-
 const peer = new Peer(undefined, {
     host : '/', 
 });
 
+const grid = document.getElementById("grid");
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
 peer.on('open', (id) => {
     socket.emit('enter-call', RID, id);
-})
-
-socket.on('user-connected', (userId) => {
-    console.log(userId);
 });
+
+navigator.mediaDevices.getUserMedia({
+    video : true, 
+    audio : true
+}).then((stream )=> {
+    addStream(myVideo, stream);
+
+    socket.on('user-connected', (userId) => {
+        connectUser(userId, stream);
+    });
+
+    peer.on('call', (call) => {
+        call.answer(stream);
+        const oldVideo = document.createElement('video');
+        call.on('stream', stream => {
+            addStream(oldVideo, stream);
+        })
+    });
+});
+
+const addStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', ()=> {
+        video.play();
+    });
+
+    grid.append(video);
+}
+
+const connectUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const newVideo = document.createElement('video');
+    call.on('stream', stream => {
+        addStream(newVideo, stream);
+    });
+    call.on('close', ()=> {
+        newVideo.remove();
+    });
+}
