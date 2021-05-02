ac382e3023460d978df108674a786c188d6de53b
PratikGarai <pratikgarai0208@gmail.com>
2021-05-02 13:00:23 +0530

    Redirect to new room using uuid

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+const { v4 } = require("uuid");
+
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
+app.get('/', (req, res, next) => {
+
+    const roomId = v4();
+    return res.redirect(`/${roomId}`)
+
+});
+
+app.get('/:roomId', (req, res, next)=> {
+
+    return res.render("chatRoom", {roomId : req.params.roomId});
+
+});
+
+server.listen(5000);
